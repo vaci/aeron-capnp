2333e807417ebe6bc2496fe37907dba0df010466
Vaci <vaci@vaci.org>
2024-02-17 12:33:37 +0000

    remove capnp overlay

--- default.nix
+++ default.nix
@@ -1,46 +1,17 @@
-let
-  capnproto_overlay = (self: super: {
-    capnproto = super.capnproto.overrideAttrs (prev: {
-      version = "git";
-      src = self.fetchFromGitHub {
-        owner = "capnproto";
-        repo = "capnproto";
-        rev = "c264e357d6f0304d04fb1267066d7d6ec7436bb3";
-        sha256 = "sha256-p8E74KoxD9wZ1+2qpalfF/SQs1Axeq4pNWoX+BIW18I=";
-      };
-    });
-   });
-in {
-  pkgs ? import <nixpkgs> {
-    #config.replaceStdenv = { pkgs }: pkgs.clangStdenv;
-    #overlays = [capnproto_overlay];
-  }
+{
+  pkgs ? import <nixpkgs> {}
 , stdenv ? pkgs.stdenv
 , debug ? true
+, aeron-cpp ? pkgs.aeron-cpp
 }:
 
-   
 let
   name = "aeron-capnp";
 
   add-links = ''
     ln --symbolic --force --target-directory=src \
       "${pkgs.ekam.src}/src/ekam/rules"
-
-    mkdir --parents src/compiler
-    ln --symbolic --force "${pkgs.capnproto}/bin/capnp" src/compiler/capnp
-    ln --symbolic --force "${pkgs.capnproto}/bin/capnpc-c++" src/compiler/capnpc-c++
-
-    mkdir --parents src/capnp/compat
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/byte-stream.capnp" src/capnp/compat/
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/byte-stream.h" src/capnp/compat/
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/byte-stream.c++" src/capnp/compat/
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/http-over-capnp.capnp" src/capnp/compat/
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/http-over-capnp.h" src/capnp/compat/
-    ln --symbolic --force "${pkgs.capnproto.src}/c++/src/capnp/compat/http-over-capnp.c++" src/capnp/compat/
   '';
-
-  aeron-cpp = import ./aeron-cpp.nix { inherit pkgs; };
 in
 
 stdenv.mkDerivation {
